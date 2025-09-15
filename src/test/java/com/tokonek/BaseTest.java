bfc9fcd7d570f9b5badf65c3feb88e01157a5c14
Denirudini <denirudini99@gmail.com>
2025-09-15 10:42:47 +0700

    upload file

--- src/test/java/com/tokonek/BaseTest.java
+++ src/test/java/com/tokonek/BaseTest.java
@@ -3,9 +3,7 @@ package com.tokonek;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 
 import com.tokonek.listener.ExtentReportListener;
 import com.tokonek.utils.DriverUtil;
